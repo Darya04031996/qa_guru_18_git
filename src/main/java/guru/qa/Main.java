039eccc7763317f461a9711a517f43889501277e
Darya04031996 <darya.melgunova@gmail.com>
2024-11-20 22:24:28 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     private static String msg = "Hello, Dog!";
     
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println(msg);
     }
 }
